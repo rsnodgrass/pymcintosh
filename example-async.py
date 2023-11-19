186f9a557ec96ae18c2c507cf40956cdf6a28d05
Ryan Snodgrass <rsnodgrass@gmail.com>
2023-11-19 04:35:11 -0800

    update

--- example-async.py
+++ example-async.py
@@ -6,46 +6,47 @@
 #   ./example-async.py --tty socket:/remote-server:4999/
 
 import logging
-import coloredlogs
 import argparse as arg
 import asyncio
 
+import coloredlogs
+
 from pymcintosh import create_equipment_controller
-from pymcintosh.const import BAUD_RATES
 
 LOG = logging.getLogger(__name__)
-coloredlogs.install(level='DEBUG')
+coloredlogs.install(level="DEBUG")
 
 p = arg.ArgumentParser(description="RS232 client example (asynchronous)")
 p.add_argument(
-    "--url", help="pyserial supported url for communication (e.g. /dev/tty.usbserial-A501SGSZ or socket://server:4999/)", required=True
+    "--url",
+    help="pyserial supported url for communication (e.g. /dev/tty.usbserial-A501SGSZ or socket://server:4999/)",
+    required=True,
 )
-p.add_argument(
-    "--equipment", default="mcintosh", help="model (e.g. mcintosh)"
-)
+p.add_argument("--type", default="mcintosh", help="equipment type (e.g. mcintosh)")
 p.add_argument(
     "--baud",
-    type=int, default=115200, help=f"baud rate ({BAUD_RATES})",
+    type=int,
+    default=115200,
+    help=f"baud rate if local tty used (default=115200)",
 )
 args = p.parse_args()
 
-config = {
-    "baudrate": args.baud
-}
+config = {"baudrate": args.baud}
+
 
 async def main():
     equipment = create_equipment_controller(
-        args.equipment,
+        args.type,
         args.url,
         serial_config_overrides=config,
-        event_loop=asyncio.get_event_loop()
+        event_loop=asyncio.get_event_loop(),
     )
 
     print(equipment.commands())
 
     await equipment.power.off()
 
-    #exit()
+    # exit()
 
 
 asyncio.run(main())
--- example.py
+++ example.py
@@ -5,46 +5,48 @@
 #   ./example.py --tty /dev/tty.usbserial-A501SGSZ
 
 import logging
-import coloredlogs
 import argparse as arg
-from pymcintosh import create_equipment_api
-from pymcintosh.const import BAUD_RATES
+
+import coloredlogs
+
+from pymcintosh import create_equipment_controller
 
 LOG = logging.getLogger(__name__)
-coloredlogs.install(level='DEBUG')
+coloredlogs.install(level="DEBUG")
 
 p = arg.ArgumentParser(description="RS232 client example (synchronous)")
 p.add_argument(
-    "--address", help="address of communication interface (e.g. /dev/tty.usbserial-A501SGSZ or socket://server:4999/)", required=True
+    "--address",
+    help="address of communication interface (e.g. /dev/tty.usbserial-A501SGSZ or socket://server:4999/)",
+    required=True,
 )
-p.add_argument(
-    "--equipment", default="mcintosh", help="equipment type (e.g. mcintosh)"
-)
+p.add_argument("--type", default="mcintosh", help="equipment type (e.g. mcintosh)")
 p.add_argument(
     "--baud",
-    type=int, default=115200, help=f"baud rate if local tty used ({BAUD_RATES})",
+    type=int,
+    default=115200,
+    help=f"baud rate if local tty used (default=115200)",
 )
 args = p.parse_args()
 
-config = {
-    "baudrate": args.baud
-}
+config = {"baudrate": args.baud}
+
 
 def main():
-  zone = 1
-  
-  equipment = create_equipment_controller(
-        args.equipment,
-        args.url,
-        serial_config_overrides=config)
+    equipment = create_equipment_controller(
+        args.type, args.url, serial_config_overrides=config
+    )
 
-  # save the status for all zones before modifying
-  zone_status = {}
-  for zone in range(1, 9):
-      zone_status[zone] = equipment.zone_status(zone)  # save current status for all zones
-      print(f"Zone {zone} status: {zone_status[zone]}")
+    # save the status for all zones before modifying
+    zone_status = {}
+    for zone in range(1, 9):
+        zone_status[zone] = equipment.zone_status(
+            zone
+        )  # save current status for all zones
+        print(f"Zone {zone} status: {zone_status[zone]}")
 
-  equipment.all_off()
-  exit
+    equipment.all_off()
+    exit
+
 
 main()
