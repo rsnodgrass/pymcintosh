46ac10607daf6b1d9a82739652324c8b14c6a476
Ryan Snodgrass <rsnodgrass@gmail.com>
2023-12-27 23:13:09 -0800

    added back config option

--- pyavcontrol/const.py
+++ pyavcontrol/const.py
@@ -14,5 +14,6 @@ DEFAULT_MODEL_LIBRARIES = [
 CONF_COMMAND_EOL = "command_eol"
 CONF_RESPONSE_EOL = "response_eol"
 CONF_COMMAND_SEPARATOR = "command_separator"
+CONF_SERIAL_CONFIG = "rs232"
 
 CONF_THROTTLE_RATE = "min_time_between_commands"
